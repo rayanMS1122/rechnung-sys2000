820990eae10ad9c70c8b5cc2ef5cff99ad0875d0
Rayan <rayan.mohammed.sulaiman@gmail.com>
2025-12-12 15:05:32 +0100

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:reciepts/controller/screen_input_controller.dart';
 import 'package:reciepts/controller/unterschrift_controller.dart';
 import 'package:reciepts/screens/name_eingeben_screen.dart';
 import 'package:reciepts/screens/designen.dart';
+import 'package:reciepts/screens/screen_input.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -89,7 +90,7 @@ class MyApp extends StatelessWidget {
             border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
           ),
         ),
-        home: NameEingebenScreen(),
+        home: ScreenInput(),
       ),
     );
   }
--- lib/screens/unterschrft_screen.dart
+++ lib/screens/unterschrft_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
+import 'package:reciepts/constants.dart';
 import 'package:reciepts/controller/unterschrift_controller.dart';
+import 'package:reciepts/screens/settings_screen.dart';
 
 class UnterschrftScreen extends StatefulWidget {
   final title;
@@ -37,30 +40,82 @@ class _UnterschrftScreenState extends State<UnterschrftScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Unterschriften"),
-        actions: [
-          IconButton(
-            onPressed: _controller.clearSignature(widget.title == "Kunde"
-                ? _controller.kundeSignatureController
-                : _controller.monteurSignatureController),
-            icon: Icon(Icons.clear),
-          ),
-          IconButton(
-            onPressed: () {
-              widget.title == "Kunde"
-                  ? _controller.saveKundeBytesToImage(context)
-                  : _controller.saveMonteurBytesToImage(context);
+      // appBar: AppBar(
+      //   title: Text("Unterschriften"),
+      //   actions: [
+      //     IconButton(
+      //       onPressed: _controller.clearSignature(widget.title == "Kunde"
+      //           ? _controller.kundeSignatureController
+      //           : _controller.monteurSignatureController),
+      //       icon: Icon(Icons.clear),
+      //     ),
+      //     IconButton(
+      //       onPressed: () {
+      //         widget.title == "Kunde"
+      //             ? _controller.saveKundeBytesToImage(context)
+      //             : _controller.saveMonteurBytesToImage(context);
 
-              // _controller.update();
-            },
-            icon: Icon(Icons.save),
-          )
-        ],
+      //         // _controller.update();
+      //       },
+      //       icon: Icon(Icons.save),
+      //     )
+      //   ],
+      // ),
+      body: Container(
+        width: MediaQuery.sizeOf(context).width * .9,
+        height: MediaQuery.sizeOf(context).height * .9,
+        child: Column(children: [
+          _buildHeader(context),
+          widget.title == "Kunde"
+              ? _controller.kundeSignatureCanvas
+              : _controller.monteurSignatureCanvas,
+        ]),
       ),
-      body: widget.title == "Kunde"
-          ? _controller.kundeSignatureCanvas
-          : _controller.monteurSignatureCanvas,
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
+      color: AppColors.background,
+      child: SafeArea(
+        bottom: false,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Back Button
+            GestureDetector(
+              onTap: () => Navigator.maybePop(context),
+              child: Container(
+                padding: EdgeInsets.all(8.w),
+                decoration: BoxDecoration(
+                  color: AppColors.primary.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                child: Icon(
+                  Icons.arrow_back,
+                  color: AppColors.primary,
+                  size: 24.sp,
+                ),
+              ),
+            ),
+
+            // Titel zentriert
+            Expanded(
+              child: Center(
+                child: Text(
+                  "Eingabe",
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    color: AppColors.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
